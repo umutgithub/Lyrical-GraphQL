431fc4040a79a2e8c9fac31a51703699e26e423f
umut <umut.sevil.contractor@hilton.com>
2018-08-05 12:50:05 -0400

    set react-apollo/apollo-client/graphql-tag and provide

---
+++ client/components/SongList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';    //just to use strings
+import { graphql } from 'react-apollo';
+
+class SongList extends Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                SongList
+            </div>
+        );
+        
+    }
+}
+
+const query = gql`
+ {
+  songs {
+    title
+  }
+ }
+`;
+
+
+export default graphql(query)(SongList);
